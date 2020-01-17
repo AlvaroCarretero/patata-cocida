94fd2130ddb50bfbc321b3b1951ef1371523210d
Alejandro-Arribas <alex2019dam@gmail.com>
2020-01-17 11:56:51 +0100

    Primer mensaje
    solo puse un sout

---
+++ GHalex/src/ghalex/GHalex.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ghalex;
+
+/**
+ *
+ * @author alex2
+ */
+public class GHalex {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("HOLA");
+    }
+    
+}
